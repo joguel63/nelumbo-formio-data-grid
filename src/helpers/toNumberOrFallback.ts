6efb0d0d764a3af2630062b28efe722776594db3
joguel63 <80803870+joguel63@users.noreply.github.com>
2023-08-15 08:37:53 -0500

    Fixed cero in the header name when the value is ""

--- src/helpers/toNumberOrFallback.ts
+++ src/helpers/toNumberOrFallback.ts
@@ -1,4 +1,5 @@
 export const toNumberOrFallback = (value: string): number | string => {
+  if (value === undefined || value === null || value === "") return value;
   const number = Number(value);
   return !isNaN(number) ? number : value;
 };
